4b422a53328ca0339bb3f667ade0019e47305c65
kosds <Kos-dan@mail.ru>
2017-10-06 19:07:56 +0300

    Other graphs

--- multistage.py
+++ multistage.py
@@ -2,53 +2,59 @@ import math
 import numpy
 from matplotlib import pyplot
 
-STAGES_COUNT = 3
 U = 3000.0
 LAMBDA = 0.1
-FUEL_CONST = 0.4
 MP = 42.0
 V = 9000.0
-M0 = MP * math.pow((1 - LAMBDA) / (math.exp(-V / (STAGES_COUNT * U)) - LAMBDA),
-                   STAGES_COUNT)
-STAGE_MASS = (M0 - MP) / STAGES_COUNT
 
 
-def fuel(mt, time):
-    mass = mt - FUEL_CONST * time
-    return mass if mass > 0.0 else 0.0
+def velocity(start_velocity, stage, masses, fuel_mass):
+    start_mass = MP + sum(masses[stage - 1:])
+    current_mass = start_mass - masses[stage - 1] * (1 - LAMBDA) + fuel_mass
+    return start_velocity + U * math.log(start_mass / current_mass)
 
 
-def velocity(fuel_mass, stage, v0):
-    const_mass = MP + (STAGES_COUNT - stage + LAMBDA) * STAGE_MASS
-    m0 = const_mass + (1 - LAMBDA) * STAGE_MASS
-    return v0 + U * math.log(m0 / (fuel_mass + const_mass))
+def get_alpha(v, stages_count):
+    p = math.exp(-v / (stages_count * U))
+    return (1 - LAMBDA) / (p - LAMBDA)
 
 
-MT = (1 - LAMBDA) * STAGE_MASS
-STAGE_TIME = int(MT / FUEL_CONST)
-t_list = numpy.arange(0, STAGES_COUNT * STAGE_TIME)
-v_list = []
-changes = []
+def get_stages_masses(mp, v, stages_count):
+    alpha = get_alpha(v, stages_count)
+    m0 = mp * alpha ** stages_count
+    m1 = m0 * (1 - 1 / alpha)
+    m2 = m0 / alpha - mp * alpha
+    m3 = (alpha - 1) * mp
+    return [m1, m2, m3]
+
+
+m0_list = [MP * get_alpha(V, x) ** x for x in range(2, 10)]
+pyplot.figure(1)
+pyplot.plot(range(2, 10), m0_list, "bo", linestyle="dashed")
+pyplot.xlabel("stages")
+pyplot.ylabel("start mass")
+pyplot.title("mp = {}, v = {}km/s".format(MP, V / 1000))
+
+STAGES_COUNT = 3
+stages = get_stages_masses(MP, V, STAGES_COUNT)
+fuel_masses = [(1 - LAMBDA) * mass for mass in stages]
 stage = 1
-v0 = 0
-time_shift = 0
-i = 0
-while i < len(t_list):
-    fuel_mass = fuel(MT, t_list[i] - time_shift)
-    if fuel_mass != 0:
-        v_list.append(velocity(fuel_mass, stage, v0))
-        i += 1
-        continue
-    time_shift = stage * STAGE_TIME
-    stage += 1
-    v0 = max(v_list)
-    changes.append(t_list[i])
-
-pyplot.plot(t_list, v_list)
-for t in changes:
-    pyplot.plot([t, t], [min(v_list), max(v_list)], "r--")
-pyplot.xlabel("Time")
-pyplot.ylabel("Velocity")
-pyplot.title("m0 = {:.2f} kg, v_max = {:.2f} km/s"
-             .format(M0, max(v_list) / 1000))
+f_list = numpy.arange(sum(fuel_masses), 0, -1)
+v_list = []
+previous_v = 0
+for f in f_list:
+    stage_fuel = f - sum(fuel_masses[stage:])
+    if stage_fuel > 0.0:
+        v_list.append(velocity(previous_v, stage, stages, stage_fuel))
+    else:
+        v_list.append(velocity(previous_v, stage, stages, 0.0))
+        stage += 1
+        previous_v = max(v_list)
+pyplot.figure(2)
+pyplot.plot(list(reversed(f_list)), v_list)
+for f in [fuel_masses[0], sum(fuel_masses[:2])]:
+    pyplot.plot([f, f], [min(v_list), max(v_list)], "r--")
+pyplot.xlabel("fuel mass")
+pyplot.ylabel("velocity")
+pyplot.title("v_max = {:.2f}km/s, ".format(max(v_list) / 1000))
 pyplot.show()
